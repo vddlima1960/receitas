3da91b261b111edd68fca6d145d41b5add5d53a7
vvdlima1960 <vddlimaredhat@gmail.com>
2023-01-10 09:28:44 -0300

    alterações de 10.01.2023'

---
+++ receitas/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    receitas = {
+        1: 'Lasanha',
+        2: 'Sopa de Legumes',
+        3: 'Sorvete'
+    }
+
+    dados = {
+        'nome_das_receitas': receitas
+    }
+    return render(request, 'index.html',dados)
+
+def receita(request):
+    return render(request, 'receita.html')
